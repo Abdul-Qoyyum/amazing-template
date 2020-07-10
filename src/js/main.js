ab73a6e8050a591924be062644266ec13712362c
Abdul-Qoyyum <nurudeen96qoyyum@gmail.com>
2020-07-10 12:45:35 -0400

    Added the portfolio and statistics section

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+// statistics counter section
+    $('#clients').animationCounter({
+        start: 0,
+        end: 3800,
+        step: 1,
+        delay: 100,
+    });
+    $('#projects').animationCounter({
+        start: 0,
+        end: 4510,
+        step: 1,
+        delay: 200,
+    });
+    $('#feedback').animationCounter({
+        start: 0,
+        end: 2800,
+        step: 1,
+        delay: 300,
+    });
+    $('#freebies').animationCounter({
+        start: 0,
+        end: 5500,
+        step: 1,
+        delay: 400,
+    });
+
+});
